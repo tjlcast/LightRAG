1f38622a74c7a7a308dd981d72e3018aec981dd7
tjlcast <phx108@sina.com>
2025-12-22 16:13:22 +0800

    Add chat history HTTP request examples

---
+++ jialtang/http/get_last_message.sh
@@ -0,0 +1,5 @@
+curl -X GET http://121.40.102.152:9966/v1/chat/history/last
+
+curl -X GET http://121.40.102.152:9966/v1/chat/history/all
+
+curl -X DELETE http://121.40.102.152:9966/v1/chat/history/all
